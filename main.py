7eda62750f988373cdfcdacc0baae7d2e95f6ccd
Ariyaraa <ariyaraa@students.dsbn.org>
2025-03-24 12:47:50 -0400

    Done

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+# Name: Akindu Ariyratna
+# Title: 
+# Date: March 19th 2025
+# Description: This program prints 0 to 100 numbers
+
+import random 
+Numbers = []
+
+for i in range(1,100):
+    number = random.randint(0,100)
+    print(number)
+    Numbers.append(number)
+
+print(sum(Numbers)/100)
 
